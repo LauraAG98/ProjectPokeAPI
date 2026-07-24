ed5ff378036a1e1b25ca78364d9e33d9da4acf3c
LauraAG98 <lauraguirregarcia@outlook.com>
2026-07-24 15:58:36 -0500

    feat: create a "Header" component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SelectorComponent } from './components/selector-component/selector-component';
+import { Header } from './components/header/header';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'app-header', component: Header },
+    {path: 'app-selector-component', component: SelectorComponent}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SelectorComponent } from './components/selector-component/selector-component';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, SelectorComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
